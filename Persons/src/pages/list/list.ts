8ce1ecf1c6da5accf6ecb6c12b63cb70b9a42590
jitups <jitusurve@hotmail.com>
2018-09-25 17:35:16 +0530

    Added infinite scroller

--- Persons/src/pages/list/list.ts
+++ Persons/src/pages/list/list.ts
@@ -8,17 +8,53 @@ import { InfoPage } from '../info/info';
   templateUrl: 'list.html'
 })
 export class ListPage {
-
-	movies: Array<any>;
-
+	//items = [];
+	//movies: Array<any>;
+	movies=[];
+	pageNumber=0;
+	maximumPages=31;
 	constructor(public navCtrl: NavController, private movieService: MovieService) {
-		this.movieService.getMovies().subscribe(
-			data=>
-			{
-				this.movies =data;
-				console.log(data);
-			});
+		this.loadMovies();
 		}
+
+		doInfinite(doInfinite?){
+			console.log('Begin async operation');
+			this.pageNumber = this.pageNumber+1;
+			this.movieService.getMoviesPageWise(this.pageNumber).subscribe(
+				data=>
+				{
+					this.movies =data;
+					console.log(data);
+				});
+				
+			// for (var i = 0; i < 10; i++) {
+			// 	this.movies.push( this.movies.length );
+			//   }
+		  }
+
+		  loadMovies(infiniteScroll?){
+			this.movieService.getMoviesPageWise(this.pageNumber).subscribe(
+				data=>
+				{
+					this.movies = this.movies.concat(data);
+					console.log(data);
+				});
+				if (infiniteScroll) {
+					infiniteScroll.complete();
+				  }
+			// for (var i = 0; i < 10; i++) {
+			// 	this.movies.push( this.movies.length );
+			//   }
+		  }
+
+		  loadMore(infiniteScroll) {
+			this.pageNumber++;
+			this.loadMovies(infiniteScroll);
+		 
+			if (this.pageNumber === this.maximumPages) {
+			  infiniteScroll.enable(false);
+			}
+		  }
 	
 	selectMovie(event, movie) {
 		console.log(movie);  
--- Persons/src/services/rest/movie-service.ts
+++ Persons/src/services/rest/movie-service.ts
@@ -11,24 +11,22 @@ export class MovieService {
     }
 
     getMovies() {
-        // const headerDict = {
-        //     'Content-Type': 'application/json',
-        //     'Accept': 'application/json',
-        //     'Authorization': this.make_base_auth("webappuser", "Jitu@123")
-        //   }
-          
-        //   const requestOptions = {                                                                                                                                                                                 
-        //     headers: new Headers(headerDict), 
-        //   };
-
         let headers = new Headers();
         headers.append('Authorization', this.make_base_auth("webappuser", "Jitu@123"));
 
-        //let options = new RequestOptions({headers: headers});
-        
-
         //var url = 'http://api.themoviedb.org/3/search/movie?query=&query=' + encodeURI("test") + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
         var url = 'https://mvcwebapi-215708.appspot.com/api/Movie';
+        var response = this.http.get(url,{headers:headers}).map(res => res.json());
+        return response;
+    }
+
+    getMoviesPageWise(pageId) {
+        let headers = new Headers();
+        headers.append('Authorization', this.make_base_auth("webappuser", "Jitu@123"));
+
+        //var url = 'http://api.themoviedb.org/3/search/movie?query=&query=' + encodeURI("test") + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+        //var url = 'https://mvcwebapi-215708.appspot.com/api/Movie?pageId='+pageId;
+        var url = 'https://localhost:44331/api/Movie/GetPagewise/'+pageId;
         var response = this.http.get(url,{headers:headers}).map(res => res.json());
         return response;
     }
